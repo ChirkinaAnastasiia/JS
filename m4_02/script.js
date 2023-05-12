1ff1ac908027c0d08ec18f26ac1904f7a0039ea7
ChirkinaAnastasia <chirkina.developer@gmail.com>
2023-05-12 20:14:27 +0300

    commit

--- m4_02/script.js
+++ m4_02/script.js
@@ -1,13 +1,15 @@
 'use strict';
 
-const productName = 'Кекс';
-let productQuantity = 7;
-const productCategory = 'Выпечка';
-let productPrice = 100;
+const productName = prompt('Введите наименование товара:');
+let productQuantity = Number(prompt('Введите количество товара:'));
+const productCategory = prompt('Введите категорию товара:');
+let productPrice = Number(prompt('Введите стоимость товара:'));
 let totalPrice = productQuantity * productPrice;
 
+console.log('productName: ', typeof productName);
+console.log('productQuantity: ', typeof productQuantity);
+console.log('productCategory: ', typeof productCategory);
+console.log('productPrice: ', typeof productPrice);
 
-console.log('productName: ', productName);
-console.log('totalPrice: ', totalPrice);
-// или так
-console.log('totalPrice: ', productQuantity * productPrice);
+console.log(`На складе ${productQuantity} единиц товара "${productName}" на сумму ${totalPrice} рублей.`);
+
