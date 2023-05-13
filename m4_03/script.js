c39bfc06defdc382eda952f37a47dd1940601733
ChirkinaAnastasia <chirkina.developer@gmail.com>
2023-05-13 21:38:31 +0300

    commit

---
+++ m4_03/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// первая задача
+const productName = prompt('Введите наименование товара:');
+let productQuantity = Number(prompt('Введите количество товара:'));
+
+if (Number.isFinite(productQuantity) === true) {
+  const productCategory = prompt('Введите категорию товара:');
+  let productPrice = Number(prompt('Введите стоимость товара:'));
+    if (Number.isFinite(productPrice) === true) {
+      let totalPrice = productQuantity * productPrice;
+      console.log(`На складе ${productQuantity} единиц товара "${productName}" на сумму ${totalPrice} рублей.`);
+    } else {
+      alert('Вы ввели некорректные данные!');
+    }
+} else {
+  alert('Вы ввели некорректные данные!');
+}
---
+++ m4_03/tasks.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// вторая задача
+const rain = Math.round(Math.random() * 1);
+
+console.log(rain);
+
+if (rain === 1) {
+  console.log('Пошёл дождь. Возьмите зонт!');
+}
+
+if (rain === 0) {
+  console.log('Дождя нет!');
+}
+
+// третья задача
+const pointsMath = +(prompt('Введите кол-во баллов по математике:'));
+const pointsRussian = +(prompt('Введите кол-во баллов по русскому языку:'));
+const pointsInformatic = +(prompt('Введите кол-во баллов по информатике:'));
+const freeEducation = pointsMath + pointsRussian + pointsInformatic;
+// так
+if (freeEducation >= 265) {
+  alert('Поздравляю, вы поступили на бюджет!');
+} else {
+  alert('К сожалению вам не хватило баллов!');
+}
+// или так
+alert(freeEducation >= 265 ? 'Поздравляю, вы поступили на бюджет!' : 'К сожалению вам не хватило баллов!');
+
+// червертая задача
+const howMuchMoney = +(prompt('Сколько денег хотете снять?'));
+// так
+if (howMuchMoney % 100 === 0) {
+  console.log('Возьмите Ваши деньги!');
+} else {
+  console.log('К сожалению, мы не можем выдать такую сумму!');
+}
+// или так
+console.log(howMuchMoney % 100 === 0 ? 'Возьмите Ваши деньги!' : 'К сожалению, мы не можем выдать такую сумму!');
