0aced9dda064f2b4398fd049b4e977ed8510fa91
ChirkinaAnastasia <chirkina.developer@gmail.com>
2023-09-09 13:15:48 +0300

    commit

--- m4_07/game_02.js
+++ m4_07/game_02.js
@@ -1,84 +1,135 @@
 'use strict';
 
-// alert(`Сыгрем в игру! Введи два числа от 1 до 100. Я загадаю число в этом диапазоне, а ты попробуешь его отгадать!`);
+const playGame = () => {
+  alert(`Сыгрем в игру! Введи два числа от 1 до 100. Я загадаю число в этом диапазоне, а ты попробуешь его отгадать!`);
 
-const getFirstNumber = () => {
-  let first = prompt(`Введи первое число:`);
-
-  if (first === null) {
-    return alert(`Конец игры!`);
-  } 
-
-  while (Number.isNaN(+first) || first === '' || !(first.trim()) || +first < 1 || +first > 100) {
-    alert(`Некорректные данные! Нужно ввести число от 1 до 100!`);
-    first = prompt(`Попробуй ещё раз! Введи первое число:`);
-
+  // получаем первое число от игрока
+  const getFirstNumber = () => {
+    let first = prompt(`Введи первое число:`);
+  
     if (first === null) {
-      return alert(`Конец игры!`);
+      return;
+    } 
+  
+    while (Number.isNaN(+first) || first === '' || !(first.trim()) || +first < 1 || +first > 100) {
+      alert(`Некорректные данные! Нужно ввести число от 1 до 100!`);
+      first = prompt(`Попробуй ещё раз! Введи первое число:`);
+  
+      if (first === null) {
+        return;
+      }
     }
-  }
-
-  return +first;
-};
-
-const firstNumber = getFirstNumber();
-console.log(`firstNumber :`, firstNumber);
-
-
-const getSecondNumber = () => {
-  let second = prompt(`Введи второе число:`); 
-
-  if (second === null) {
-    return alert(`Конец игры!`);
-  } 
-
-  while (Number.isNaN(+second) || second === '' || !(second.trim()) || +second < 1 || +second > 100 || +second === firstNumber) {
-    alert(`Некорректные данные! Нужно ввести число от 1 до 100 не равное первому числу!`);
-    second = prompt(`Попробуй ещё раз! Введи первое число:`);
-
+  
+    return +first;
+  };
+  
+  //получаем второе число от игрока
+  const getSecondNumber = () => {
+    let second = prompt(`Введи второе число:`); 
+  
     if (second === null) {
       return alert(`Конец игры!`);
+    } 
+  
+    while (Number.isNaN(+second) || second === '' || !(second.trim()) || +second < 1 || +second > 100 || +second === firstNumber) {
+      alert(`Некорректные данные! Нужно ввести число от 1 до 100, не равное первому числу!`);
+      second = prompt(`Попробуй ещё раз! Введи второе число:`);
+  
+      if (second === null) {
+        return alert(`Конец игры!`);
+      }
     }
+  
+    return +second;
+  };
+  
+  // считаем рандомное число  между первым и вторым
+  const getRandomNumber = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+  
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+  
+  // считаем количество попыток в диапазоне
+  const getAttemptsNumber = () => {
+    let range = firstNumber > secondNumber ? firstNumber - secondNumber + 1 : secondNumber - firstNumber + 1;
+    // if (firstNumber > secondNumber) {
+    //   range = firstNumber - secondNumber + 1;
+    // } else {
+    //   range = secondNumber - firstNumber + 1;
+    // }
+    let attempts = range >= 50 && range <= 100 ? 15 : Math.round(range * 0.3);
+    // if (range >= 50 && range <= 100) {
+    //   attempts = 15;
+    // } else {
+    //  attempts = Math.round(range * 0.3);
+    // }
+  
+    return attempts;
+  };
+  
+  // начинаем игру
+  const startGame = () => {
+    const answersArray = [];
+  
+    alert(`Я загадал число. Псс... это ${randomNumber}! Попробуй его угадать. Количество попыток - ${attemptsNumber}.`);
+  
+    for (let i = 0; i < attemptsNumber; i++) {
+  
+      let playerAnswer = prompt(`Введи свой вариант:`);
+  
+      if (answersArray.includes(+playerAnswer)) {
+        alert(`Ты уже вводил это число, попытка не засчитывается!`);
+        i--;
+  
+        continue;
+      }
+  
+      if (playerAnswer === null) {
+        return alert(`Конец игры!`);
+      }
+  
+      if (+playerAnswer === randomNumber) {
+        return alert(`Правильно! Поздравляю, ты выиграл!`);
+      }
+  
+      if (Number.isNaN(+playerAnswer) || playerAnswer === '' || !(playerAnswer.trim())
+        || +playerAnswer < (firstNumber > secondNumber ? secondNumber : firstNumber) || +playerAnswer > (firstNumber > secondNumber ? firstNumber : secondNumber)) {
+  
+        alert(`Некорректные данные. Введи число из указанного диапазона. Попытка не засчитывается!`);
+        i--;
+
+        continue;
+      }
+
+      if (i === attemptsNumber - 1) {
+        return alert(`Попытки закончились. Ты проиграл!`);
+      }
+  
+      if (playerAnswer > randomNumber) { 
+        alert(`Больше!`);
+        answersArray.push(+playerAnswer);
+  
+      } else if (playerAnswer < randomNumber) {
+        alert(`Меньше!`);
+        answersArray.push(+playerAnswer);
+      }
+    }
+  };
+  
+  const firstNumber = getFirstNumber();
+  
+  // если нет первого числа, второе не спрашивает
+  if (firstNumber === undefined) {
+    return  alert(`Конец игры!`);
   }
-
-  return +second;
+  
+  const secondNumber = getSecondNumber();  
+  const randomNumber = getRandomNumber(firstNumber, secondNumber);  
+  const attemptsNumber = getAttemptsNumber();
+  
+  startGame();
 };
 
-const secondNumber = getSecondNumber();
-console.log(`secondNumber :`, secondNumber);
-
-
-const getRandomNumber = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-const randomNumber = getRandomNumber(firstNumber, secondNumber);
-console.log(`randomNumber :`, randomNumber);
-
-
-const getAttemptsNumber = () => {
-  let range = firstNumber > secondNumber ? firstNumber - secondNumber + 1 : secondNumber - firstNumber + 1;
-  console.log(`range :`, range);
-  // if (firstNumber > secondNumber) {
-  //   range = firstNumber - secondNumber + 1;
-  // } else {
-  //   range = secondNumber - firstNumber + 1;
-  // }
-  let attempts = range >= 50 && range <= 100 ? 15 : Math.round(range * 0.3);
-  // if (range >= 50 && range <= 100) {
-  //   attempts = 15;
-  // } else {
-  //  attempts = Math.round(range * 0.3);
-  // }
-
-  return attempts;
-};
-
-const attemptsNumber = getAttemptsNumber();
-console.log(`attemptsNumber :`, attemptsNumber);
-
-
-
+playGame();
