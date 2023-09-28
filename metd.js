28551b46c76200eb4ff058292de4a2d1b124167b
ChirkinaAnastasia <chirkina.developer@gmail.com>
2023-09-28 19:03:38 +0300

    commit

--- metd.js
+++ metd.js
@@ -3479,3 +3479,144 @@
 ///////////////////////////                       ///////////////////////////
 //                             Модуль 4 Урок 12                            //
 ///////////////////////////                       ///////////////////////////
+
+// const x = 5;
+// console.log(x);
+
+// const scope = () => {
+//   // console.log(x);
+//   const x = 10;
+//   console.log(x);
+
+//   const scope2 = () => {
+//     const x = 15;
+//     console.log(x);
+//   };
+//   scope2();
+// };
+
+// scope();
+// console.log(x);
+
+////////////////////////////////
+// const x = 5;
+
+// const scopeOne = (y, z) => {
+//   // lexicalEnvironment = {y: 15, z: undefined}
+//   const x = 10;
+//   // lexicalEnvironment = {x: 10, y: 15, z: undefined}
+//   console.log(x, y, z);
+//   const scopeTwo = () => {
+//   // lexicalEnvironment = {}
+//   // scope = scopeOne.lexicalEnvironment = {x: 10, y: 15, z: undefined}
+//     console.log(x);
+//   };
+//   scopeTwo();
+// };
+
+// scopeOne(15);
+
+////////////////////////////////
+// const x = 5;
+
+// const scopeOne = (y, z) => {
+//   // lexicalEnvironment = {y: 15, z: undefined}
+//   // scope = GlobalScope = {x: 5, scopeOne} 
+//   console.log(y, z);
+//   const scopeTwo = () => {
+//   // lexicalEnvironment = {}
+//   // scope = scopeOne.lexicalEnvironment = {y: 15, z: undefined}
+//     console.log(x);
+//   };
+//   scopeTwo();
+// };
+
+// scopeOne(15);
+
+////////////////////////////////
+// let x = 5;
+
+// const scopeOne = (y) => {
+//   console.log(x + y);
+// };
+// scopeOne(10);
+// scopeOne(7);
+
+// // x = 4;
+// const scopeTwo = () => {
+//   x = 20;
+//   // let x = 20;
+//   scopeOne(10);
+//   scopeOne(3);
+// };
+// scopeTwo();
+// scopeOne(3);
+
+////////////////////////////////
+// function sum(a) {
+
+//   return function(b) {
+//     return a + b;
+//   };
+// };
+// console.log( sum(4)(3) );
+
+// let a = 5;
+// function foo(){
+//   console.log(a);
+//   // a = 7;
+//   let a = 7;
+// }
+// foo();
+
+////////////////////////////////
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+
+// function inBetween(a, b) {
+//   return function(x) {
+//     return x >= a && x <= b;
+//   };
+// };
+// console.log( arr.filter(inBetween(2, 5)) );
+
+// function inArray(arr) {
+//   return function(x) {
+//     return arr.includes(x);
+//   };
+// };
+// console.log( arr.filter(inArray([1, 5, 99, 23])) );
+
+////////////////////////////////
+// let users = [
+//   { name: "John", age: 20, surname: "Johnson" },
+//   { name: "Pete", age: 18, surname: "Peterson" },
+//   { name: "Ann", age: 19, surname: "Hathaway" }
+// ];
+
+// // const sortedByName = users.sort((a, b) => a.name > b.name ? 1 : -1);
+// // console.log(sortedByName);
+// // const sortedByAge = users.sort((a, b) => a.age > b.age ? 1 : -1);
+// // console.log(sortedByAge);
+
+// function byField(fieldName) {
+//   return (a, b) => a[fieldName] > b[fieldName] ? 1 :-1;
+// };
+// console.log(users.sort(byField('age')));
+// // console.log(users.sort(byField('name')));
+
+////////////////////////////////
+
+function outer() {
+  // область видимости функции outer()
+  let v = 'Я из области видимости функции outer()!'
+
+  function inner() {
+      // область видимости функции inner()
+      console.log(v) // 'Я из области видимости функции outer()!'
+  }
+
+  return inner
+}
+
+const f = outer()
+f()
